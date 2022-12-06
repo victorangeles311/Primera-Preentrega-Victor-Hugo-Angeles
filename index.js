dcebb6fb4870e53f710cbf78f129c6b2e61b9080
Víctor Angeles <angeles.v311@outlook.com>
2022-12-05 20:23:59 -0600

    Simulador interactivo v.1.2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-//SIMULADOR INTERACTIVO: Cálculo de costo total de productos y/o servicios seleccionados por el usuario.
+//SIMULADOR INTERACTIVO
+
 
 /*
     Login
     Nombre de usuario: victor_311
     Contraseña: 92306
 */
+
 
 let nombreUsuario = prompt("¡Bienvenido a Synth World! Ingresa tu nombre de usuario");
 
@@ -17,40 +19,62 @@ let contraseña = parseInt(prompt(`Hola ${nombreUsuario} ingresa tu contraseña 
 
 while (contraseña != 92306){
     alert("Contraseña incorrecta");
-    contraseña = prompt("Intenta nuevamente");
+    contraseña = parseInt(prompt("Intenta nuevamente"));
 }
 
 alert("¡Bienvenido Víctor!");
 
 
+
 /*
-    Agregar artículos al carrito
+    Agregar artículos al carrito y resultados con descuentos
 */
 
 let cuerdas = 1200;
 let percusiones = 450;
 let pianos = 350;
 
-let preguntaC = prompt(`¿Desea agregar el Paquete de Sonidos de Cuerdas? Costo: ${cuerdas}`) .toLowerCase();
+const resta = (a,b) => a - b;
+const descuento = c => c * 0.15;
+
+
+alert("Vamos a confirmar tus compras")
+let preguntaC = prompt(`¿Seguro que desea agregar el Paquete de Sonidos de Cuerdas? Costo: ${cuerdas}`) .toLowerCase();
 
 if(preguntaC === "si") {
     alert("Agregado al carrito");
+    let costoTotal = resta(cuerdas, descuento(cuerdas));
+    alert(`¡Felicidades, este producto tiene un descuento del 15%! Total = ${costoTotal}`);
+} else if (preguntaC === "no") {
+    alert ("Retirado del carrito");
 } else {
-    alert ("Ok");
+    alert ("Producto guardado")
 }
 
-let preguntaP = prompt(`¿Desea agregar el Paquete de Sonidos de Percusiones? Costo: ${percusiones}`) .toLowerCase();
+
+
+let preguntaP = prompt(`¿Seguro que desea agregar el Paquete de Sonidos de Percusiones? Costo: ${percusiones}`) .toLowerCase();
 
 if(preguntaP === "si") {
     alert("Agregado al carrito");
+} else if (preguntaP === "no") {
+    alert ("Retirado del carrito");
 } else {
-    alert ("Ok");
+    alert ("Producto guardado")
 }
 
-let preguntaPi = prompt(`¿Desea agregar el Paquete de Sonidos de Pianos? Costo: ${pianos}`) .toLowerCase();
+
+
+let preguntaPi = prompt(`¿Seguro que desea agregar el Paquete de Sonidos de Pianos? Costo: ${pianos}`) .toLowerCase();
 
 if(preguntaPi === "si") {
     alert("Agregado al carrito");
+    let costoTotal = resta(pianos, descuento(pianos));
+    alert(`¡Felicidades, este producto tiene un descuento del 15%! Total = ${costoTotal}`);
+} else if (preguntaPi === "no") {
+    alert ("Retirado del carrito");
 } else {
-    alert ("Ok");
+    alert ("Producto guardado")
 }
+
+alert("Gracias por tu preferencia");
